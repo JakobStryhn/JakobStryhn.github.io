ed48e317ced2ae777ad81bbcc6d9d2f79bd43823
JakobStryhn <JakobStryhn@users.noreply.github.com>
2025-07-31 08:56:03 +0000

    deploy: 35ad35cdd7b1b0aa933a4ddc327fd5e13635e93f

---
+++ js/app.js
@@ -0,0 +1,35 @@
+﻿window.downloadFileFromByteArray = (filename, contentType, byteArray) => {
+    const blob = new Blob([byteArray], { type: contentType });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+};
+
+window.downloadResumeFile = async (englishresume) => {
+
+    var fileName
+    if (englishresume) {
+        fileName = "Jakob-Min-Stryhn-Resume.pdf"
+    }
+    else {
+        fileName = "Jakob-Min-Stryhn-CV.pdf"
+    }
+
+    response = await fetch(`/files/${fileName}`)
+
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob)
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+};
